4a29c46a03efb24d30db01dc0d6b8ac387b105fa
Mansionair <momynov.sultan@gmail.com>
2020-06-27 18:36:09 +0600

    refactor: add users container component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,129 +1,78 @@
 import React from 'react';
 import styles from './Users.module.css';
-import axios from 'axios';
 import userIcon from '../../assets/img/user-icon.svg';
 import ActionButton from '../Elements/Button';
 
-export default class Users extends React.Component {
-  componentDidMount() {
-    if (this.props.users.length === 0) {
-      // `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
-      axios
-        .get(`http://localhost:3004/users`, {
-          params: {
-            _page: this.props.currentPage,
-            _limit: this.props.pageSize,
-          },
-        })
-        .then((response) => {
-          this.props.setTotal(response.headers['x-total-count']);
-          this.props.setUsers(response.data);
-        });
-    }
+// Pure functional components that only takes props and returns JSX
+const Users = (props) => {
+  const pagesCount = Math.ceil(props.totalPages / props.pageSize);
+
+  const pages = [];
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
   }
 
-  onPageChange = (page) => {
-    this.props.setCurrentPage(page);
-    axios
-      .get(`http://localhost:3004/users`, {
-        params: {
-          _page: page,
-          _limit: this.props.pageSize,
-        },
-      })
-      .then((response) => {
-        this.props.setTotal(response.headers['x-total-count']);
-        this.props.setUsers(response.data);
-      });
-  };
-
-  render() {
-    if (this.props.users.length === 0) {
-      return (
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            paddingTop: '60px',
-            fontSize: '20px',
-          }}
-        >
-          Loading...
-        </div>
-      );
-    }
-
-    const pagesCount = Math.ceil(this.props.totalPages / this.props.pageSize);
-
-    const pages = [];
-    for (let i = 1; i <= pagesCount; i++) {
-      pages.push(i);
-    }
-
-    return (
-      <div className={styles.usersContainer}>
-        <div className={styles.pagesContainer}>
-          <strong>total: {pagesCount}</strong> <br />
-          {pages.map((page) => {
-            return (
-              <span
-                key={page}
-                className={
-                  this.props.currentPage === page
-                    ? styles.selectedPage
-                    : 'false'
-                }
-                // We pass an anonymous function there because we need a pageNumber parameter, but default onClick function takes and event parameter, which we don't need
-                onClick={() => {
-                  this.onPageChange(page);
-                }}
-              >
-                {page}
-              </span>
-            );
-          })}
-        </div>
-        {this.props.users.map((user) => {
+  return (
+    <div className={styles.usersContainer}>
+      <p>Pages:</p>
+      <div className={styles.pagesContainer}>
+        {pages.map((page) => {
           return (
-            <div key={user.id} className={styles.userInfoContainer}>
-              <div className={styles.userInfoLeftBar}>
-                <p>{user.name}</p>
-                <img
-                  className={styles.userPhoto}
-                  src={
-                    user.photos.small !== null ? user.photos.small : userIcon
-                  }
-                  alt=''
-                />
-                <div className={styles.actionButton}>
-                  {user.followed ? (
-                    <ActionButton
-                      className={styles.unfollowButton}
-                      name='Unfollow'
-                      onClick={() => {
-                        this.props.unfollow(user.id);
-                      }}
-                    />
-                  ) : (
-                    <ActionButton
-                      className={styles.followButton}
-                      name='Follow'
-                      onClick={() => {
-                        this.props.follow(user.id);
-                      }}
-                    />
-                  )}
-                </div>
-              </div>
-              <div className={styles.userStatusBar}>{user.status}</div>
-              <div className={styles.userLocationBar}>
-                <p>{'user.location.city'}</p>
-                <p>{'user.location.country'}</p>
-              </div>
-            </div>
+            <span
+              key={page}
+              className={
+                props.currentPage === page ? styles.selectedPage : 'false'
+              }
+              // We pass an anonymous function there because we need a pageNumber parameter, but default onClick function takes and event parameter, which we don't need
+              onClick={() => {
+                props.onPageChange(page);
+              }}
+            >
+              {page}
+            </span>
           );
         })}
       </div>
-    );
-  }
-}
+      {props.users.map((user) => {
+        return (
+          <div key={user.id} className={styles.userInfoContainer}>
+            <div className={styles.userInfoLeftBar}>
+              <p>{user.name}</p>
+              <img
+                className={styles.userPhoto}
+                src={user.photos.small !== null ? user.photos.small : userIcon}
+                alt=''
+              />
+              <div className={styles.actionButton}>
+                {user.followed ? (
+                  <ActionButton
+                    className={styles.unfollowButton}
+                    name='Unfollow'
+                    onClick={() => {
+                      props.unfollow(user.id);
+                    }}
+                  />
+                ) : (
+                  <ActionButton
+                    className={styles.followButton}
+                    name='Follow'
+                    onClick={() => {
+                      props.follow(user.id);
+                    }}
+                  />
+                )}
+              </div>
+            </div>
+            <div className={styles.userStatusBar}>{user.status}</div>
+            <div className={styles.userLocationBar}>
+              <p>{'user.location.city'}</p>
+              <p>{'user.location.country'}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import axios from 'axios';
 import { connect } from 'react-redux';
 import {
   followUserAC,
@@ -6,7 +8,78 @@ import {
   setTotalPagesCountAC,
   setCurrentPageAC,
 } from '../../redux/users-reducer';
-import Users from './Users';
+import Users from './Users.jsx';
+
+/**
+ * Now there are 2 container components and 1 functional
+ * First container component's purpose is to connect React with Redux Store
+ * Second container component's purpose is to make API requests and give props to Users functional component
+
+ */
+
+// Second container component
+class UsersContainer extends React.Component {
+  componentDidMount() {
+    if (this.props.users.length === 0) {
+      // `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      axios
+        .get(`http://localhost:3004/users`, {
+          params: {
+            _page: this.props.currentPage,
+            _limit: this.props.pageSize,
+          },
+        })
+        .then((response) => {
+          this.props.setTotal(response.headers['x-total-count']);
+          this.props.setUsers(response.data);
+        });
+    }
+  }
+
+  onPageChange = (page) => {
+    this.props.setCurrentPage(page);
+    axios
+      .get(`http://localhost:3004/users`, {
+        params: {
+          _page: page,
+          _limit: this.props.pageSize,
+        },
+      })
+      .then((response) => {
+        this.props.setTotal(response.headers['x-total-count']);
+        this.props.setUsers(response.data);
+      });
+  };
+
+  render() {
+    if (this.props.users.length === 0) {
+      return (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            paddingTop: '60px',
+            fontSize: '20px',
+          }}
+        >
+          Loading...
+        </div>
+      );
+    }
+
+    return (
+      <Users
+        currentPage={this.props.currentPage}
+        pageSize={this.props.pageSize}
+        totalPages={this.props.totalPages}
+        users={this.props.users}
+        onPageChange={this.onPageChange}
+        follow={this.props.follow}
+        unfollow={this.props.unfollow}
+      />
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
@@ -38,6 +111,5 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
-
-export default UsersContainer;
+// First container component
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
